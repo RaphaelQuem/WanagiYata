edf9dd760aae34cd445a2685f099a43e662a397a
Raphael Maia <raphael@montecarlo.com.br>
2017-06-16 15:45:09 -0300

    Resolving issues

---
+++ WanagiYata/Assets/Scripts/Extension/Vector3Extend.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Extension
+{
+    public static class Vector3Extend
+    {
+        
+        public static void AvoidCollision(this Vector3 vector,Vector3 origin, float range)
+        {
+            Quaternion rotation = Quaternion.AngleAxis(10, Vector3.up);
+            while (Physics.Raycast(origin, vector, range))
+                vector = rotation * vector;
+            //bla bla
+        }
+    }
+}
