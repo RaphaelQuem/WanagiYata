4d87174478ef5a4c537bcfbc5f3735138c526cc7
Raphael Maia <raphzmaia@gmail.com>
2017-08-01 12:29:53 -0300

    State machine modifications

--- WanagiYata/Assets/Scripts/StateMachine/PlayerStateMachine.cs
+++ WanagiYata/Assets/Scripts/StateMachine/PlayerStateMachine.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Assets.Scripts.StateMachine
 {
@@ -25,16 +26,48 @@ namespace Assets.Scripts.StateMachine
         private Vector2 directorVector;
         private bool isSettingTrap;
         private bool isRolling;
-
         private Animator animator;
-        public ObjectState CurrentState { get { return currentState; } set{ currentState = value; } }
-        public FacingDirection CurrentDirection { get { return currentDirection; } set { currentDirection = value; } }
-        public bool IsSettingTrap { get; set; }
-        public bool IsRolling { get { return isRolling; } set { isRolling = value; animator.SetBool("isRolling", IsRolling); } }
+        public Dictionary<ObjectState, float> ActionTime = new Dictionary<ObjectState, float>()
+        {
+            {ObjectState.Walking,0f},
+            {ObjectState.Idle,0f},
+            {ObjectState.SettingTrap,1f},
+            {ObjectState.Rolling,1f},
+            {ObjectState.Shooting,1f}
+        };
 
         public PlayerStateMachine(Animator anim)
         {
             animator = anim;
+        }
+        public ObjectState CurrentState { get { return currentState; } set { currentState = value; } }
+        public FacingDirection CurrentDirection { get { return currentDirection; } set { currentDirection = value; } }
+        public bool IsSettingTrap { get; set; }
+        public bool IsRolling
+        {
+            get
+            {
+                return isRolling;
+            }
+            set
+            {
+                isRolling = value;
+                animator.SetBool("isRolling", IsRolling);
+                ChangeCurrentState(ObjectState.Rolling);
+            }
+        }
+
+        public void Update()
+        {
+            currentStateDuration += Time.deltaTime;
+            float comparer = 0f;
+            ActionTime.TryGetValue(CurrentState, out comparer);
+            if (comparer > 0f && currentStateDuration > comparer)
+            {
+                ChangeCurrentState(ObjectState.Idle);
+                Directorvector = directorVector;
+            }
+
         }
 
         public Vector2 Directorvector
@@ -46,29 +79,31 @@ namespace Assets.Scripts.StateMachine
             set
             {
                 directorVector = value;
-                currentStateDuration += Time.deltaTime;
-                if (directorVector != Vector2.zero)
+                if (CurrentState != ObjectState.Rolling && CurrentState != ObjectState.SettingTrap && CurrentState != ObjectState.Shooting)
                 {
-                    animator.SetFloat("InputX", directorVector.x);
-                    animator.SetFloat("InputY", directorVector.y);
-                    animator.SetBool("isWalking", true);
-                    ChangeCurrentState(ObjectState.Walking);
-                    CurrentDirection = GetCurrentDirection();
-                }
-                else
-                {
-                    ChangeCurrentState(ObjectState.Idle);
-                    if (currentStateDuration > 0.05f)
-                        animator.SetBool("isWalking", false);
+                    if (directorVector != Vector2.zero)
+                    {
+                        animator.SetFloat("InputX", directorVector.x);
+                        animator.SetFloat("InputY", directorVector.y);
+                        animator.SetBool("isWalking", true);
+                        ChangeCurrentState(ObjectState.Walking);
+                        CurrentDirection = GetCurrentDirection();
+                    }
+                    else
+                    {
+                        ChangeCurrentState(ObjectState.Idle);
+                        if (currentStateDuration > 0.05f)
+                            animator.SetBool("isWalking", false);
+                    }
                 }
             }
         }
 
         private FacingDirection GetCurrentDirection()
         {
-            if(Mathf.Abs(directorVector.x) > Mathf.Abs(directorVector.y))
+            if (Mathf.Abs(directorVector.x) > Mathf.Abs(directorVector.y))
             {
-                return (directorVector.x > 0?FacingDirection.Right:FacingDirection.Left);
+                return (directorVector.x > 0 ? FacingDirection.Right : FacingDirection.Left);
             }
             else
             {
