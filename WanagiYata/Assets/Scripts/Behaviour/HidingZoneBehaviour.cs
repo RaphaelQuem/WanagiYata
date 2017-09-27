b811709beefcd48b93fd86d45d62d8bd98883b6e
Raphael Maia <raphzmaia@gmail.com>
2017-09-26 23:00:05 -0300

    Stealth zones implemented and stealth button added to input

---
+++ WanagiYata/Assets/Scripts/Behaviour/HidingZoneBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidingZoneBehaviour : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+
+            other.GetComponent<PlayerMovement>().CanHide = true;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+
+            other.GetComponent<PlayerMovement>().CanHide = false;
+        }
+    }
+}
---
+++ WanagiYata/Assets/Scripts/Extension/GameObjectExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Extension
+{
+    public static class GameObjectExtension
+    {
+        public static GameObject GetClosestObject(this GameObject self, GameObject[] objects)
+        {
+            GameObject result = null;
+            float minDist = Mathf.Infinity;
+            foreach (GameObject o in objects)
+            {
+                float dist = Vector3.Distance(o.transform.position, self.transform.position);
+
+                if (dist < minDist)
+                {
+                    result = o;
+                    minDist = dist;
+                }
+
+            }
+            return result;
+        }
+    }
+}
