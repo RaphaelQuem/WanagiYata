3f85e184ad8e4995a2744328873d158fd4df5ebb
Raphael Maia <raphzmaia@gmail.com>
2017-10-13 23:16:35 -0300

    Daytime implementation. Needs to find the day time duration and implement de slider

---
+++ WanagiYata/Assets/TimeControllerBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeControllerBehaviour : MonoBehaviour {
+    public float daytimeduration;
+    public int currentDay;
+    public float currentTime;
+	// Use this for initialization
+	void Start () {
+        currentDay = 1;
+        currentTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (currentTime >= 60)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("sunSprite");
+            currentTime = 0;
+        }
+        else if (currentTime > 30)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("moonSprite");
+        }
+        currentTime += Time.deltaTime;
+
+	}
+}
