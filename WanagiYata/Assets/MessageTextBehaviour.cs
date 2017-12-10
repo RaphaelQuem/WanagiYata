04d8947560f82bb273e6c59617821ae27ecb7fbb
Raphael Maia <raphzmaia@gmail.com>
2017-12-09 22:16:57 -0200

    Type writing effect message object added

---
+++ WanagiYata/Assets/MessageTextBehaviour.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageTextBehaviour : MonoBehaviour {
+
+    public Text text;
+    public bool playOnAwake = true;
+    public float delayToStart;
+    public float delayBetweenChars = 0.125f;
+    public float delayAfterPunctuation = 0.5f;
+
+    private string story;
+    private float originDelayBetweenChars;
+    private bool lastCharPunctuation = false;
+    private char charComma;
+    private char charPeriod;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        originDelayBetweenChars = delayBetweenChars;
+
+        charComma = Convert.ToChar(44);
+        charPeriod = Convert.ToChar(46);
+
+        if (playOnAwake)
+        {
+            ChangeText(text.text, delayToStart);
+        }
+    }
+
+    //Update text and start typewriter effect
+    public void ChangeText(string textContent, float delayBetweenChars = 0f)
+    {
+        StopCoroutine(PlayText()); //stop Coroutime if exist
+        story = textContent;
+        text.text = ""; //clean text
+        Invoke("Start_PlayText", delayBetweenChars); //Invoke effect
+    }
+
+    void Start_PlayText()
+    {
+        StartCoroutine(PlayText());
+    }
+
+    IEnumerator PlayText()
+    {
+
+        foreach (char c in story)
+        {
+            delayBetweenChars = originDelayBetweenChars;
+
+            if (lastCharPunctuation)  //If previous character was a comma/period, pause typing
+            {
+                yield return new WaitForSeconds(delayBetweenChars = delayAfterPunctuation);
+                lastCharPunctuation = false;
+            }
+
+            if (c == charComma || c == charPeriod)
+            {
+                lastCharPunctuation = true;
+            }
+
+            text.text += c;
+            yield return new WaitForSeconds(delayBetweenChars);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
