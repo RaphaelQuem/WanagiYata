a2d8de9d89f792c9464694eff53911780a8ff96b
Raphael Maia <raphzmaia@gmail.com>
2017-12-22 22:33:47 -0200

    Refactoring state archtecture

---
+++ WanagiYata/Assets/Scripts/StateMachine/Player/PlayerShootingState.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.StateMachine.Player
+{
+    public class PlayerIdleState : IState
+    {
+        private PlayerBehaviour _player;
+        public PlayerIdleState  (PlayerBehaviour player)
+        {
+            _player = player;
+        }
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Vector2 DirectorVector { get; set; }
+
+        public void Update()
+        {
+            if (InputManager.BPressed())
+            {
+                _player.speed = 2.5f;
+            }
+            else
+            {
+                _player.speed = 1f;
+            }
+            _player.CurrentState = new PlayerWalkingState(_player);
+        }
+    }
+}
