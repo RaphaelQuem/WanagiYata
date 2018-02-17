fa1fd6bdc44529b928839a13f3f7de9e7725196b
Raphael Maia <raphzmaia@gmail.com>
2018-02-16 22:44:43 -0200

    PlayerTalkingState implemented not allowing hero to move during dialogue and some code was refactored to its own PlayerStateClass

---
+++ WanagiYata/Assets/Scripts/StateMachine/Player/PlayerIdleState.cs
@@ -0,0 +1,45 @@
+﻿using Assets.Scripts.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.StateMachine.Player
+{
+    public class PlayerIdleState : IState
+    {
+        private PlayerBehaviour _player;
+        public PlayerIdleState  (PlayerBehaviour player)
+        {
+            Debug.Log("Idle");
+            _player = player;
+        }
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Vector2 DirectorVector { get; set; }
+
+        public void Update()
+        {
+            if (!InputManager.ControllerVector().Equals(Vector2.zero))
+                _player.CurrentState = new PlayerWalkingState(_player);
+
+            if (InputManager.AButton())
+            {
+                if (_player.CurrentAction.Equals(PlayerAction.Talk))
+                    _player.CurrentState = new PlayerTalkingState(_player);
+            }
+        }
+    }
+}
---
+++ WanagiYata/Assets/Scripts/StateMachine/Player/PlayerTalkingState.cs
@@ -0,0 +1,69 @@
+﻿using Assets.Scripts.Extension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.StateMachine.Player
+{
+    public class PlayerTalkingState : IState
+    {
+        private PlayerBehaviour _player;
+        public PlayerTalkingState(PlayerBehaviour player)
+        {
+            Debug.Log("Talking");
+            _player = player;
+            Talk();
+        }
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Vector2 DirectorVector { get; set; }
+
+        public void Update()
+        {
+            if (InputManager.AButton())
+                Talk();
+        }
+        public void Talk()
+        {
+            var x = GameObject.FindGameObjectWithTag("MessageText");
+            var z = GameObject.FindGameObjectWithTag("MessageBG");
+            var y = GameObject.FindGameObjectWithTag("MessageCanvas");
+            var t = x.GetComponent<Text>();
+            if (x != null)
+            {
+                if (x.GetComponent<MessageTextBehaviour>().story.Equals(t.text) || t.text.Equals(string.Empty))
+                {
+                    var texto = _player.ActionTarget.GetComponent<NPCBehaviour>().DialogueManager.GetText(_player);
+                    if (texto.Equals(string.Empty))
+                    {
+                        z.GetComponent<SpriteRenderer>().enabled = false;
+                        x.GetComponent<MessageTextBehaviour>().enabled = false;
+                        x.GetComponent<Text>().enabled = false;
+                        _player.CurrentState = new PlayerIdleState(_player);
+                    }
+                    else
+                    {
+                        z.GetComponent<SpriteRenderer>().enabled = true;
+                        x.GetComponent<MessageTextBehaviour>().ChangeText(texto);
+                        x.GetComponent<MessageTextBehaviour>().enabled = true;
+                        x.GetComponent<Text>().enabled = true;
+                    }
+                }
+            }
+        }
+    }
+}
